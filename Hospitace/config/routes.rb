6ab81ca185b183efa8ddb867410f96d6b6c08f10
osmman <osmman@61608b94-3092-d755-6515-bc2228ce6eaf>
2011-10-15 20:19:23 +0000

    hraní si s rails a odebrání z repozitáře tmp/*.*

--- Hospitace/config/routes.rb
+++ Hospitace/config/routes.rb
@@ -16,7 +16,8 @@ Hospitace::Application.routes.draw do
  # end
 
   controller 'courses' do
-    match 'blog/:id' => :show, :constraints => {
+    match 'courses' => :index
+    match 'courses/:id' => :show, :constraints => {
       :id => /[0-9A-Z]+/i
     }
   end
@@ -76,6 +77,6 @@ Hospitace::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  #match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
   
 end
