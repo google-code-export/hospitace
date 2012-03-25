3c114611fd37ca0221c6550968b51914f5e50b22
osmman@gmail.com <osmman@gmail.com@61608b94-3092-d755-6515-bc2228ce6eaf>
2012-03-25 10:42:17 +0000

    FIX - načítání modulu pro překlad modelů

--- Hospitace/lib/dynamic_form.rb
+++
@@ -1,9 +0,0 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
-require 'action_view/helpers/dynamic_form'
-
-class ActionView::Base
-  include DynamicForm
-end
-
--- Hospitace/lib/kosapi/model_helpers.rb
+++ Hospitace/lib/kosapi/model_helpers.rb
@@ -8,26 +8,22 @@ module KOSapi
   module ModelHelpers  
     
     def self.included(base)
-      base.send(:include, InstanceMethods)
-      base.send(:extend, ClassMethods)
+      base.instance_eval do
+        extend  ClassMethods
+        include InstanceMethods
+      end
     end
     
     module ClassMethods
-      def self.extended(base)
-        def self.extended(base)
-          base.class_eval do
-            def self.attrs_translate(*args)    
-              args.each do |attribute|
-                define_method "#{attribute}_t" do
-                  value = self.send(attribute)
-                  value = value.first if value.is_a?(Array)
-                  scope = self.class.name.demodulize.downcase
-                  KOSapi::I18n.t value, :scope=>".#{scope}.#{attribute}", :default=>value
-                end    
-              end
-            end
-          end
-        end 
+      def attrs_translate(*args)    
+        args.each do |attribute|
+          define_method "#{attribute}_t" do
+            value = self.send(attribute)
+            value = value.first if value.is_a?(Array)
+            scope = self.class.name.demodulize.downcase
+            KOSapi::I18n.t value, :scope=>".#{scope}.#{attribute}", :default=>value
+          end    
+        end
       end
     end
     module InstanceMethods
