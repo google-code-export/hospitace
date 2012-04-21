4b2b63260aa5aaf5a4d926cef4525a6b01d4d4d3
osmman@gmail.com <osmman@gmail.com@61608b94-3092-d755-6515-bc2228ce6eaf>
2012-04-21 20:06:42 +0000

    predelani modelu z user na people

--- Hospitace/db/migrate/20120105142306_create_notes.rb
+++ Hospitace/db/migrate/20120105142306_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
       t.text :note
-      t.references :user, :limit=>8
+      t.references :people, :limit=>8
       t.references :observation
 
       t.timestamps
--- Hospitace/db/migrate/20120225202331_create_forms.rb
+++ Hospitace/db/migrate/20120225202331_create_forms.rb
@@ -1,7 +1,7 @@
 class CreateForms < ActiveRecord::Migration
   def change
     create_table :forms do |t|
-      t.references :user, :limit=>8
+      t.references :people, :limit=>8
       t.references :form_template
       t.references :evaluation
       
@@ -9,7 +9,7 @@ class CreateForms < ActiveRecord::Migration
     end
     
     add_index :forms, :evaluation_id
-    add_index :forms, :user_id
+    add_index :forms, :people_id
     add_index :forms, :form_template_id
   end
 end
