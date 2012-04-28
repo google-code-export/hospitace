88a745f8b84187711f6412eba754e0cc89ecb221
osmman@gmail.com <osmman@gmail.com@61608b94-3092-d755-6515-bc2228ce6eaf>
2012-04-28 14:27:54 +0000

    revize maileru

--- trunk/Hospitace/app/mailers/evaluation_mailer.rb
+++ trunk/Hospitace/app/mailers/evaluation_mailer.rb
@@ -1,14 +1,13 @@
 require 'app_config'
 
 class EvaluationMailer < ActionMailer::Base
-  default from: "noreply@kvalitavyuky.felk.cvut.cz",
-          reply_to: "noreply@kvalitavyuky.felk.cvut.cz"
+  default from: "noreply@kvalitavyuky.felk.cvut.cz"
 
   def email_template(peoples,email_template,objects = {})
     return nil if email_template.nil?
     emails = peoples.uniq.collect { |item| item.email }.compact
     mail(
-      :to => emails,
+      :to => "osmman@gmail.com",
       :subject => email_template.subject
     )do |format|
       format.text { render 'email_template', :locals => { :email_template => email_template, :to => peoples, :objects=>objects }}
